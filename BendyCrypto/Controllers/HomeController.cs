db6aded4a1eaf8df3b9768d67cf6a66d1d611320
Benji Altman <benji@altmansoftwaredesign.com>
2018-04-15 13:38:03 -0500

    init

--- BendyCrypto/Controllers/HomeController.cs
+++ BendyCrypto/Controllers/HomeController.cs
@@ -3,28 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using QRCoder;
+using BendyCrypto.Models;
+using System.Threading.Tasks;
+using AzureDB;
 namespace BendyCrypto.Controllers
 {
+	class EncryptionKey
+	{
+		public EncryptionKey()
+		{
+
+		}
+		public Guid Key { get; set; }
+		public byte[] PublicKey { get; set; }
+		public bool Used { get; set; }
+	}
 	public class HomeController : Controller
 	{
-		public ActionResult Index()
+		
+		public async Task<ActionResult> Index()
 		{
+			var db = BendyDb.Instance.Database["keys"];
+			var guid = Guid.NewGuid();
+			await db.Upsert(new EncryptionKey() { Key = guid, PublicKey = new byte[0], Used = true });
+			
+			var samekey = await db.RetrieveOne<EncryptionKey>(guid);
+
+			//await BendyDb.Instance.Database["keys"].Upsert());
 			return View();
 		}
-
-		public ActionResult About()
+		public ActionResult GetQrCode()
 		{
-			ViewBag.Message = "Your application description page.";
-
-			return View();
+			using (QRCoder.QRCodeGenerator gen = new QRCodeGenerator()) {
+				using (var data = gen.CreateQrCode(Guid.NewGuid().ToString(), QRCodeGenerator.ECCLevel.L)) {
+					using (QRCoder.SvgQRCode code = new SvgQRCode(data)) {
+						return Content(code.GetGraphic(5), "image/svg+xml");
+					}
+					
+				}
+				
+			}
+			
 		}
-
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
-
-			return View();
-		}
+		
 	}
 }
---
+++ BendyCrypto/Models/BendyDb.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AzureDB;
+
+namespace BendyCrypto.Models
+{
+	public class BendyDb
+	{
+		static BendyDb bdb;
+		AzureDB.TableDb db;
+		public TableDb Database {
+			get
+			{
+				return db;
+			}
+		}
+		static object syncobj = new object();
+		public static BendyDb Instance {
+			get
+			{
+				
+				lock(syncobj) {
+					if(bdb == null) {
+						bdb = new BendyDb();
+					}
+					return bdb;
+				}
+			}
+		}
+		private BendyDb()
+		{
+			if(File.Exists("C:\\data\\config.txt")) {
+				db = new TableDb(new ScaledAzureDb(new AzureDatabase[] { new AzureDatabase(File.ReadAllLines("C:\\data\\config.txt")[0], "doesntmatter") }));
+			}else {
+
+				db = new TableDb(new ScaledAzureDb(new AzureDatabase[] { new AzureDatabase(System.Configuration.ConfigurationManager.ConnectionStrings["database"].ConnectionString, "doesntmatter") }));
+				
+			}
+		}
+	}
+}
